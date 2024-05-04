f223c6d7fbd0b0af1d35f19ce12a240fc61296a4
andy840119 <andy840119@gmail.com>
2024-05-04 23:15:03 +0800

    fix(config): P2.3 - should change the import path in the config.

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,3 @@
-import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
+import { addMatchImageSnapshotCommand } from '@simonsmith/cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand();
